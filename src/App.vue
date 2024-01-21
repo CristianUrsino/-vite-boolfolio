ee00343da37d855cdf4c993d2fd80b7612e63d86
CristianUrsino <ursino.cristian.uc@gmail.com>
2024-01-21 14:47:26 +0100

    added basic algorithm

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,35 @@
 <template>
+  <div>ciao</div>
+  <ul>
+    <li v-for="project in projects">
+      {{project.name}}
+    </li>
+  </ul>
 </template>
 
 <script>
+import {store} from "./store";
+import axios from "axios";
+export default{
+  name: 'App',
+  data(){
+    return{
+      store,
+      projects:[],
+    };
+  },
+  methods: {
+    getAllProjects(){
+      axios.get(store.apiUrl + "/project", {params: {page: this.currentPage}}).then((res) => {
+        this.projects = res.data.results.data;
+        console.log(this.projects);
+      });
+    },
+  },
+  mounted(){
+    this.getAllProjects();
+  }
+}
 </script>
 
 <style>
