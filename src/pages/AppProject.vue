449df5bb9e2350e9f800d130d9933f33e85b393b
CristianUrsino <ursino.cristian.uc@gmail.com>
2024-01-22 17:34:06 +0100

    updated algorithm of AppProject

--- src/pages/AppProject.vue
+++ src/pages/AppProject.vue
@@ -1,24 +1,77 @@
 <template>
-    <h1>PROGETTI</h1>
+  <div>
+    <router-view></router-view>
+    <h1>PROGETTI:</h1>
+    <ul>
+      <li v-for="project in projects" :key="project.id">
+        {{ project.name }}
+      </li>
+    </ul>
+    <button class="btn" :class="{ 'disabled': !nextButtonStatus }" @click="nextPage" :disabled="!nextButtonStatus">
+      avanti
+    </button>
+    <button class="btn" :class="{ 'disabled': !prevButtonStatus }" @click="previousPage" :disabled="!prevButtonStatus">
+      indietro
+    </button>
+  </div>
 </template>
-    
-<script>
-    import {store} from "../store";
-    import axios from "axios";
-    export default{
-      name: 'App',
-      data(){
-        return{
-          store,
-        };
+  
+  <script>
+  import {store} from "../store";
+  import axios from "axios";
+  export default{
+    name: 'App',
+    data(){
+      return{
+        store,
+        projects:[],
+        currentPage: 1,
+        prevButtonStatus:false,
+        nextButtonStatus:true,
+        lastPage: 0,
+      };
+    },
+    methods: {
+      getAllProjects(){
+        axios.get(store.apiUrl + "/project", {params: {page: this.currentPage}}).then((res) => {
+          this.projects = res.data.results.data;
+          // console.log(this.projects);
+          this.currentPage = res.data.results.current_page;
+          this.lastPage = res.data.results.last_page;
+          if(this.lastPage === 1){
+            this.nextButtonStatus = false;
+          }
+        });
       },
+      nextPage(){
+        console.log('next');
+        this.prevButtonStatus = true;
+        this.currentPage = this.currentPage +1;
+        this.getAllProjects();
+        if (this.currentPage === this.lastPage) {
+          this.nextButtonStatus = false;
+        }
+      },
+      previousPage(){
+        console.log('prev');
+        this.nextButtonStatus = true;
+        this.currentPage = this.currentPage -1;
+        this.getAllProjects();
+        if (this.currentPage === 1) {
+          this.prevButtonStatus = false;
+        }
+      }
+    },
+    mounted(){
+      this.getAllProjects();
     }
-</script>
-    
-<style lang="scss" scoped>
-//RICORDARE ASSETS
-@use "../assets/style/partials/variables" as *;
-h1{
+  }
+  </script>
+  
+  <style lang="scss" scoped>
+  //RICORDARE ASSETS
+  @use "../assets/style/partials/variables" as *;
+  h1{
     color: $font-color;
-}
-</style>
+  }
+  </style>
--- src/router.js
+++ src/router.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import AppHome from "./pages/AppHome.vue";
-// import AppProject from "./pages/AppProject.vue";
+import AppProject from "./pages/AppProject.vue";
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
