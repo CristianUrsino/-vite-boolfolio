e6dcb495cb5558d7a0a3cdec4627819b2b0073e2
CristianUrsino <ursino.cristian.uc@gmail.com>
2024-01-24 17:42:59 +0100

    updated ContactForm

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -1,13 +1,71 @@
 <template>
-    
+    <form @submit.prevent="submitForm()">
+        <div class="mb-3">
+            <label for="name" class="form-label">Name</label>
+            <input type="text" class="form-control" id="name" aria-describedby="nameHelp" v-model="name">
+            <div id="nameHelp" class="form-text">Insert your name</div>
+        </div>
+        <div class="mb-3">
+            <label for="email" class="form-label">email</label>
+            <input type="email" class="form-control" id="email" aria-describedby="emailHelp" v-model="email">
+            <div id="emailHelp" class="form-text">insert your email</div>
+        </div>
+        <div class="mb-3">
+            <label for="address" class="form-label">address</label>
+            <input type="text" class="form-control" id="address" aria-describedby="addressHelp" v-model="address">
+            <div id="addressHelp" class="form-text">insert your address</div>
+        </div>
+        <div class="mb-3">
+            <label for="message" class="form-label">message</label>
+            <textarea class="form-control" id="message" aria-describedby="messageHelp" v-model="message">
+            </textarea>
+            <div id="messageHelp" class="form-text">insert your message</div>
+        </div>
+        
+        <button type="submit" class="btn btn-primary me-3">Submit</button>
+        <button type="reset" class="btn btn-info">reset</button>
+    </form>
 </template>
+  
+  <script>
+  import axios from 'axios';
+import { store } from '../store.js';
 
-<script>
-export default{
-    name:'ContactForm',
-}
-</script>
+export default {
+    name: 'ContactForm',
+    data() {
+        return {
+            name: '',
+            email: '',
+            address: '',
+            message: '',
+        };
+    },
+    methods: {
+        submitForm() {
+            const formData = {
+                name: this.name,
+                email: this.email,
+                address: this.address,
+                message: this.message,
+            };
 
-<style lang="scss" scoped>
-    
-</style>
+            axios.post(`${store.apiUrl}/contacts`, formData)
+                .then((res) => {
+                    console.log(res.data);
+                    this.name = '';
+                    this.email = '';
+                    this.address = '';
+                    this.message = '';
+                })
+                .catch((err) => {
+                    console.error(err);
+                });
+            }
+        }
+    };
+  </script>
+  
+  <style lang="scss" scoped>
+
+  </style>
