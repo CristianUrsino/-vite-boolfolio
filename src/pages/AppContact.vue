797114ba5e8b3203ed4903fd2d12e07b1a7637f9
CristianUrsino <ursino.cristian.uc@gmail.com>
2024-01-24 17:38:13 +0100

    added AppContact and started ContactForm

---
+++ src/pages/AppContact.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+      <h1>Contattaci</h1>
+      <ContactForm />
+    </div>
+  </template>
+  
+  <script>
+  import { store } from '../store.js';
+  import ContactForm from '../components/ContactForm.vue';
+  
+  export default {
+    name: 'AppContact',
+    data() {
+      return {
+        store,
+      };
+    },
+    components: {
+      ContactForm,
+    },
+  };
+  </script>
+  
+  <style lang="scss" scoped>
+  </style>
